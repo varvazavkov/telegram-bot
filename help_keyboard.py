59035d9c4856f9f9c7437bd7dd7c29f79accbafa
varvazavkov <138797264+varvazavkov@users.noreply.github.com>
2023-07-07 14:30:03 +0300

    help_keyboard.py

--- help_keyboard.py
+++ help_keyboard.py
@@ -4,5 +4,3 @@ HELP_COMMAND = """
 <b>/create_room</b> - <em>добавить комнату</em>
 <b>/participants</b> - <em>получить список участников</em>
 <b>/join_room</b> - <em>присоединиться к комнате</em>"""
-
-start = "Hello! I'm a bot to create a queue. \nTo create new room please type /create_room. \nIf you need help - type /help"
